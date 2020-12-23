844f57c56bf48171ddc7a31fd9c43621aa366319
Detre Dominus <detre.dominus@centralpoint.nl>
2020-12-23 11:36:38 +0100

    Updates

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir .my-sql-dumps
+
+touch .my-ugly-test.php \
+      .my-payload-i-captured.json \
+      .my-logs-from-last-execution.log \
+      .my-custom-deploy-or-init.sh
+
+docker-compose up -d
